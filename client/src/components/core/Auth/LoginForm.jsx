352b94b0df74af872bda58359c059d7b3c3aa47c
Ayush Priyam <ayushpriyam24@gmail.com>
2024-08-06 23:53:54 +0530

    created navbar

--- client/src/components/core/Auth/LoginForm.jsx
+++ client/src/components/core/Auth/LoginForm.jsx
@@ -1,8 +1,46 @@
-function LoginForm(){
-    return(
-        <div>
-         Bnao bhai
-        </div>
-    )
-}
-export default LoginForm
+import React, { useState } from "react";
+import { Input } from "@nextui-org/react";
+import EyeFilledIcon from "../../../assets/openeye.png";
+import EyeSlashFilledIcon from "../../../assets/closeeye.svg";
+
+export const LoginForm = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => setIsVisible(!isVisible);
+  return (
+    <div className="w-[400px] h-[400px] bg-white bg-opacity-60 drop-shadow-2xl shadow-2xl rounded-lg flex flex-col items-center text-white">
+      <Input
+        type="email"
+        variant="underlined"
+        label="Email"
+        className="max-w-xs text-white"
+      />
+      <Input
+        label="Password"
+        variant="underlined"
+        endContent={
+          <button
+            className="focus:outline-none"
+            type="button"
+            onClick={toggleVisibility}
+            aria-label="toggle password visibility"
+          >
+            {isVisible ? (
+              <img
+                src={EyeSlashFilledIcon}
+                className="h-4 text-default-400 pointer-events-none "
+              />
+            ) : (
+              <img
+                src={EyeFilledIcon}
+                className="h-4 text-default-400 pointer-events-none"
+              />
+            )}
+          </button>
+        }
+        type={isVisible ? "text" : "password"}
+        className="max-w-xs text-white"
+      />
+    </div>
+  );
+};
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,12 +1,20 @@
-export default function Login(){
-    return (
-        <div className="bg-[#303841] h-screen">
-           <div className="w-[50%]">
-           <div className="text-2xl drop-shadow-lg font-bold m-2 text-[#ef6407]">SLICE<span className="text-[#F5E8E4]">MATE</span></div>
-           </div>
-           <div className="w-[50%]">
-            
-           </div>
+import signupimg from "../assets/signup2.png";
+import signinimg from "../assets/signin.svg";
+import { LoginForm } from "../components/core/Auth/LoginForm";
+export default function Login() {
+  return (
+    <div className=" h-screen">
+      <div className="flex flex-col md:flex-row justify-center items-center min-h-screen ">
+        <div className="md:w-1/2 flex justify-center ">
+          <img
+            src={signinimg}
+            className="drop-shadow-custom-black h-[400px]  md:h-[400] lg:ml-28"
+          />
         </div>
-    )
+        <div className="md:w-1/2 flex justify-center text-white bg-[#303841]  ">
+          <LoginForm />
+        </div>
+      </div>
+    </div>
+  );
 }
